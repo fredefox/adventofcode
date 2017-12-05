c8e4e5354a22dceb9f45d58e562b76b618e13a80
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-12-05 18:05:12 +0100

    Unfinished day3

--- day-3/Main.hs
+++ day-3/Main.hs
@@ -23,3 +23,23 @@ main = print . solve . rephrase $ input
 rephrase = pred
 
 input = 325489
+
+test = take 3 $ part2 ([1, 2, 4, 5, 10, 11, 13, 25], 2)
+
+part2 :: ([Int], Int) -> [[Int]]
+part2 (xs, n) = rest:(part2 (rest, n+1))
+  where
+    lxs = length xs
+    (side, dist) = factor lxs n
+    rest :: [Int]
+    rest = scanl step (last xs) [0..8*n-2]
+    step :: Int -> Int -> Int
+    step prev ix = prev + neighborsFromBefore ((ix + n') `mod` (8 * n))
+    n' = n - 1
+    neighborsFromBefore ix
+      = xs !! (side * n') + dist
+-- one-off might occur here:
+--      + xs !! (side * n') + dist - 1
+
+factor :: Int -> Int -> (Int, Int)
+factor n m = (n `div` m, n `mod` m)
