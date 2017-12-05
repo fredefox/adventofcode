268f431e1d2875753743f7b8c7d5fba7cdfd9468
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-12-05 21:11:54 +0100

    Day 5 - part 1

---
+++ day-5/DList.hs
@@ -0,0 +1,56 @@
+module DList
+  ( DList()
+  , fromList
+  , forward
+  , backward
+  , stepForward
+  , stepBackward
+  , step
+  , current
+  , modifyCurrent
+  ) where
+
+import Data.Monoid
+import Control.Monad ((>=>))
+
+data DList a = DList [a] [a] deriving (Eq, Show)
+
+instance Monoid (DList a) where
+  mempty = DList mempty mempty
+  (DList l0 r0) `mappend` (DList l1 r1) = DList l0 (r0 <> reverse l1 <> r1)
+
+fromList :: [a] -> DList a
+fromList = DList mempty
+
+forward :: DList a -> Maybe (DList a)
+forward (DList l r) = case r of
+  [] -> Nothing
+  x:xs -> Just $ DList (x:l) xs
+
+backward :: DList a -> Maybe (DList a)
+backward (DList l r) = case l of
+  [] -> Nothing
+  x:xs -> Just $ DList xs (x:r)
+
+step, stepForward, stepBackward :: Integer -> DList a -> Maybe (DList a)
+step n
+  | n < 0 = stepBackward (-n)
+  | n > 0 = stepForward n
+  | n == 0 = return
+
+stepForward n = repeatM n forward
+stepBackward n = repeatM n backward
+
+current :: DList a -> Maybe a
+current (DList _ xs) = case xs of
+  [] -> Nothing
+  x:_ -> Just x
+
+modifyCurrent :: (a -> a) -> DList a -> DList a
+modifyCurrent f dl@(DList l r) = case r of
+  [] -> dl
+  x:xs -> DList l ((f x):xs)
+
+repeatM :: Monad m => Integer -> (a -> m a) -> a -> m a
+repeatM 0 _ = return
+repeatM n act = act >=> repeatM (n-1) act
---
+++ day-5/Main.hs
@@ -0,0 +1,86 @@
+import qualified Data.List as L
+import DList (DList(), current, modifyCurrent)
+import qualified DList as DL
+import Control.Monad.State
+import Control.Monad.Except
+
+import Debug.Trace
+
+main :: IO ()
+main = getContents >>= print . solve . parse
+
+parse :: String -> DList Int
+parse = DL.fromList . map read . lines
+
+solve :: DList Int -> Int
+solve = either id err . runSolver solver
+  where
+    err = error "should never happen"
+
+runSolver :: Solver a -> DList Int -> Either Int (Int, DList Int)
+runSolver s = runExcept . execStateT s . initEnv
+
+solver :: Solver ()
+solver = forever evalStep
+
+evalStep :: Solver ()
+evalStep = do
+  -- record one step
+  -- read and increment current pointer
+  n <- readCurrent
+  -- get list; step n times in that list; update list-state
+  getList >>= stepM n >>= putList
+
+stepM :: Int -> DList a -> Solver (DList a)
+stepM n l = liftMaybeCurr (DL.step (fromIntegral n) l)
+
+-- Records that a step has been taken
+incr :: Solver ()
+incr = modify incr'
+  where
+    incr' (n, x) = (succ n, x)
+
+getList :: Solver (DList Int)
+getList = gets snd
+
+putList :: DList Int -> Solver ()
+putList l = modify putList'
+  where
+    putList' (n, _) = (n, l)
+
+modList :: (DList Int -> DList Int) -> Solver ()
+modList f = modify putList'
+  where
+    putList' (n, l) = (n, f l)
+
+getCurrent :: Solver Int
+getCurrent = getList >>= liftMaybeCurr . current
+
+-- If an 'error' occurs, just exit with the current amount of steps.
+liftMaybeCurr :: Maybe a -> Solver a
+liftMaybeCurr m = do
+  n <- gets fst
+  liftMaybe n m
+
+liftMaybe :: Int -> Maybe a -> Solver a
+liftMaybe alt Nothing = throwError alt
+liftMaybe _ (Just a) = return a
+
+modCurrent :: (Int -> Int) -> Solver ()
+modCurrent f = do
+  l <- getList
+  putList $ modifyCurrent f l
+
+readCurrent :: Solver Int
+readCurrent = do
+  -- n <- (\x -> trace ("Moving " ++ (show x)) x) <$> getCurrent
+  n <- getCurrent
+  modCurrent succ
+  incr
+  return n
+
+-- terminates with the amount of steps on out-of-bounds
+type Solver a = StateT (Int, DList Int) (Except Int) a
+
+initEnv :: DList Int -> (Int, DList Int)
+initEnv = (,) 0
